808ce8f58f17e03b13e1b108b6fb2fbcfc29c5bd
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-10-05 09:44:34 +1000

    Initial program set up with suggested actions to be developed.

--- Rig.py
+++ Rig.py
@@ -1,8 +1,23 @@
 """
 File: Rig.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Program to manage Rig class as part of COMP1048 Basic Programming Assessment
+Author: Catherine Jackson
+ID: 110481962
+Username: JACCA010
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Rig:
+    def __init__(self):
+        self.__rig_name = ""    # Linked to hacker name?
+        self.__damage = 0
+        self.__condition = ""    # Based on damage level
+        self.__storage = ""    # Rig inventory with default inclusions
+        self.__upgrade_level = 0    # default level
+        self.__max_damage = 2
+
+    # repair action to be added
+    # upgrade action to be added
+    # transfer of inventory between rig and hacker to be added
+    # asset generation (random) to be added
+    # rig damage to be added max damage = 2
