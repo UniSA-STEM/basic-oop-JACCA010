5793a198fc20e63ce92e91382f4805b735ceb19a
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-10-05 09:38:09 +1000

    Initial Asset.py set up with suggested actions to be developed.

--- Asset.py
+++ Asset.py
@@ -1,8 +1,19 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Program to manage Asset class as part of COMP1048 Basic Programming Assessment
+Author: Catherine Jackson
+ID: 110481962
+Username: JACCA010
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self):
+        self.__asset_name:  ["CryptoToken", "Data Spike", "Removable Drive", "Security Chip", "Hardware Patch"]
+        self.__asset_description: []    # list of lists?
+        self.__attribute:  "Unencrypted"    # default setting
+        self.__inventory_type: ""    # hacker or rig inventory
+
+    # asset descriptions as per COMP1048 instructions
+    # actions to be created to move between hacker and rig inventory lists
+    # actions to be created to add or delete certain assets based on utilisation
