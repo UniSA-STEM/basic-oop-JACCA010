5a08eba0e5663528c1bc88927847794232d82829
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-10-05 09:29:30 +1000

    Initial Hacker.py set up with suggested actions to be developed.

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,19 @@
 """
 File: Hacker.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Program to manage Hacker class as part of COMP1048 Basic Programming Assessment
+Author: Catherine Jackson
+ID: 110481962
+Username: JACCA010
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+class Hacker:
+    def __init__(self):
+        self.__name = ""  # possible random name creation?
+        self.__inventory = ""  # get asset?
+        self.__trace_level = 0  # exposure level base line
+
+    # action - rig acquisition
+    # action - launch data spike
+    # action - encrypt assets
+    # action - rig upgrade
+    # action - store and retrieve assets / scan inventory
