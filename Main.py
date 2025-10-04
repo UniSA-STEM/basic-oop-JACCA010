e21f104ca69b53426d20c261ceb354091a8ad3f8
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-10-05 09:46:28 +1000

    Initial program set up with suggested actions to be developed.

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 """
 File: main.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Program to manage the classes and test program as part of COMP1048 Basic Programming Assessment
+Author: Catherine Jackson
+ID: 110481962
+Username: JACCA010
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+# import classes
+# simulate battles, upgrades, encryption and trace management
+# manage test cases to verify program
