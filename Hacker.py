7b1def31676fa0a31589ecfb3fac15cbe1fbe98c
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-10-06 12:27:35 +1000

    String method updated. Acquire rig method added.

--- Hacker.py
+++ Hacker.py
@@ -7,13 +7,30 @@ Username: JACCA010
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 class Hacker:
+    def __str__(self):  # string method added
+        output = f"{self._name}, {self.__rig}, {self.__trace_level}, {self.__inventory}"
+        return output
+
     def __init__(self, name, rig, trace_level, inventory):
         self._name = []  # possible random name creation?
         self.__rig = []  # consider option to use hacker name?
-        self.__trace_level = 0  # exposure level base line
+        self.__trace_level = 0  # exposure level baseline
         self.__inventory = ""  # get asset?
 
-    # action - rig acquisition
+    def acquire_rig (self):    # method to initiate rig for new hacker
+        if self.__rig == None:
+            input (f"You must first acquire a rig. A rig will cost you one CryptoToken. Do you want to proceed? (Y/N)")
+            if input() == "Y":
+                print (f"Rig activated. Rig name {self.__rig} assigned.")
+            else:
+                input (f"You must first acquire a rig. A rig will cost you one CryptoToken. Do you want to proceed? (Y/N)")
+        else:
+            return self.__rig
+
+
+
+
+
     # action - launch data spike
     # action - encrypt assets
     # action - rig upgrade
